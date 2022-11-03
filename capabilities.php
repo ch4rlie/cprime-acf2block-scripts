00818222a47980e5a6635cf45ab47a948c451c89
Charlie <charlie@charliegrove.com>
2022-11-03 12:53:50 -0700

    file

---
+++ capabilities.php
@@ -0,0 +1,38 @@
+<?php
+
+
+/**
+ * Process the Hero
+ */
+
+$hero_block = '<!-- wp:group {"className":"capabilities-hero"} -->
+<div class="wp-block-group capabilities-hero"><!-- wp:group {"className":"left"} -->
+<div class="wp-block-group left"><!-- wp:group {"className":"top-logo"} -->
+<div class="wp-block-group top-logo"><!-- wp:image {"id":73661,"sizeSlug":"large","linkDestination":"media"} -->
+<figure class="wp-block-image size-large"><a href="http://cprime.test/wp-content/uploads/2022/11/Aha_White_BlueBG.png"><img src="http://cprime.test/wp-content/uploads/2022/11/Aha_White_BlueBG-1024x512.png" alt="" class="wp-image-73661"/></a></figure>
+<!-- /wp:image --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"className":"text"} -->
+<div class="wp-block-group text"><!-- wp:heading {"level":1} -->
+<h1 id="h-product-strategy-w-aha-and-cprime">Product Strategy w/ Aha! and Cprime</h1>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Give product teams their own space to focus on strategy. Build roadmaps, connect them to Jira and deliver faster.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button {"className":"is-style-fill"} -->
+<div class="wp-block-button is-style-fill"><a class="wp-block-button__link">Try it free ></a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"className":"right"} -->
+<div class="wp-block-group right"><!-- wp:image {"id":73662,"width":449,"height":337,"sizeSlug":"large","linkDestination":"media"} -->
+<figure class="wp-block-image size-large is-resized"><a href="http://cprime.test/wp-content/uploads/2022/11/aha-hero.jpg"><img src="http://cprime.test/wp-content/uploads/2022/11/aha-hero-1024x769.jpg" alt="" class="wp-image-73662" width="449" height="337"/></a></figure>
+<!-- /wp:image --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->';
